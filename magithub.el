bf9fd88e6819ce01d7c9de05ef80ab859ab87ac9
Nathan Weizenbaum <nex342@gmail.com>
2010-06-06 17:15:27 -0700

    Add an empty skeleton file.

---
+++ magithub.el
@@ -0,0 +1,19 @@
+;;; magithub.el --- Magit extensions for using GitHub
+
+;; Copyright (c) 2010 Nathan Weizenbaum
+
+;; Author: Nathan Weizenbaum
+;; URL: http://github.com/nex3/magithub
+;; Version: 0.1
+;; Created: 2010-06-06
+;; By: Nathan Weizenbaum
+;; Keywords: git, github, magit
+;; Package-Requires: ((magit "0.8"))
+
+;;; Commentary:
+;; This package extends magit.el with various functions for using GitHub.
+
+
+(provide 'magithub)
+
+;;; magithub.el ends here
