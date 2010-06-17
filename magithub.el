db9df114a92a84131290dd541e3a69d847635d44
Nathan Weizenbaum <nex342@gmail.com>
2010-06-17 02:35:21 -0700

    Minor text improvement.

--- magithub.el
+++ magithub.el
@@ -15,7 +15,7 @@
 
 ;; This package does two things.  First, it extends Magit's UI with
 ;; assorted GitHub-related functionality, similar to the github-gem
-;; tool (http://github.com/defunkt/github-gem).  Second, Magithub uses
+;; tool (http://github.com/defunkt/github-gem).  Second, it uses
 ;; Magit's excellent Git library to build an Elisp library for
 ;; interfacing with GitHub's API.
 
